5418903318b2c2200bc7cb0ce0d90c4acbdff3bb
Hector2809 <arellano.reyes.hector28@gmail.com>
2024-09-20 00:28:40 -0600

    git commit -m "Second commit

--- validar.js
+++ validar.js
@@ -3,31 +3,31 @@ function validar(form){
     //validar nombre
     let nombre=form.nombre.value.trim()
     if(nombre==""){
-        alert("Por mí, ingresa un nombre válido")
+        alert("Por favor, ingresa un nombre válido")
         return false
     }
 
     //validar edad
     let edad=form.edad.value.trim()
     if(edad===""|| isNaN(edad)|| edad<=0){
-        alert("Por mí, ingrese una edad válida")
+        alert("Por favor, ingrese una edad válida")
         return false
     }
 
     //validar sexo
     let sexo=form.sexo.value
     if(sexo==""){
-        alert("Por mí, ingrese un sexo")
+        alert("Por favor, ingrese un sexo")
         return false
     }
 
     //validar deporte
     let deporte=form.deporte.value
     if(deporte=="ninguno"){
-        alert("Por mí, ingrese un deporte")
+        alert("Por favor, ingrese un deporte")
         return false
     }
 
     //si todo es válido
-    return true
+    return alert("Datos correctos")
 }
